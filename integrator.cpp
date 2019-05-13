6bbcd3c74cf27771d2c73ecba7def0bdb9564afd
andierodin@gmail.com <andierodin@gmail.com>
2019-05-13 23:51:59 +0300

    Add Integrator for using the Solar System

---
+++ integrator.cpp
@@ -0,0 +1,19 @@
+#include "integrator.h"
+//Перегрузка оператора
+// Разобраться с циклом
+void Integrator(std::vector<Earth> &Earth)
+{
+  const  double Tend = 365*86400;
+  const double Tbeginning= 1;
+  const double deltaT=86400;
+  for (  double i =Tbeginning;  i<Tend/deltaT; ++i){
+    for (auto j : Earth){
+      j.Forces();
+      j.currentCoordinates_.x+=j.gravityForces_.x*deltaT;
+      j.currentCoordinates_.y+=j.gravityForces_.y*deltaT;
+      j.GravityShow();
+
+    }
+  }
+
+}
---
+++ integrator.h
@@ -0,0 +1,10 @@
+#ifndef INTEGRATOR_H
+#define INTEGRATOR_H
+#include "planets.h"
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+
+void Integrator ( std::vector <Earth> & Earth);
+
+#endif // INTEGRATOR_H
